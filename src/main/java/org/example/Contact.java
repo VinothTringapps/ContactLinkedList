2142719900980ce529f06bcf3c43cda987d60ff8
Tringapps-User20 <vinothkumarp.pukalenthi@tringapps.com>
2023-02-13 16:16:22 +0530

    upload successfully

--- src/main/java/org/example/Contact.java
+++ src/main/java/org/example/Contact.java
@@ -97,35 +97,38 @@ public class Contact {
         Scanner sc=new Scanner(System.in);
         Logger log= Logger.getLogger("Contact");
         int ch;
+        String s1="Enter the Name :";
+        String s2="Enter the Email :";
+        String s3="Enter the Number :";
        do{
            log.info("1.InsertFirst \n 2.InsertLast\n3.InsertAtPos\n4.RemoveFirst\n5.RemoveLast\n6.RemoveAtPos\n7.Display");
            log.info("Enter the Choice :");
            ch= sc.nextInt();
            switch (ch){
                case 1->{
-                   log.info("Enter the Name :");
+                   log.info(s1);
                   String nameInsertFirst= sc.next();
-                  log.info("Enter the Email :");
+                  log.info(s2);
                   String emailInsertFirst=sc.next();
-                  log.info("Enter the Number :");
+                  log.info(s3);
                   String numberInsertFirst=sc.next();
                   l.insertFirst(nameInsertFirst,emailInsertFirst,numberInsertFirst);
                }
                case 2->{
-                   log.info("Enter the Name :");
+                   log.info(s1);
                    String nameInsertLast= sc.next();
-                   log.info("Enter the Email :");
+                   log.info(s2);
                    String emailInsertLast=sc.next();
-                   log.info("Enter the Number :");
+                   log.info(s3);
                    String numberInsertLast=sc.next();
                    l.insertLast(nameInsertLast,emailInsertLast,numberInsertLast);
                }
                case 3->{
-                   log.info("Enter the Name :");
+                   log.info(s1);
                    String nameInsertAt=sc.next();
-                   log.info("Enter the Email :");
+                   log.info(s2);
                    String emailInsertAt= sc.next();
-                   log.info("Enter the Number :");
+                   log.info(s3);
                    String numberInsertAt=sc.next();
                    log.info("Enter the Position :");
                    int index=sc.nextInt();
